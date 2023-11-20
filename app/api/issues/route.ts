8db9f2370a8c2863be3d6bc26908b31f2051e2d8
Moshtaq <moshtaqramazan@gmail.com>
2023-11-20 15:12:01 +0100

    bulid an api for creating an issue

---
+++ app/api/issues/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+import { z } from 'zod';
+
+const createIssueSchema = z.object({
+    title: z.string().min(1).max(255),
+    description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+    try {
+        const body = await request.json();
+        const validation = createIssueSchema.safeParse(body);
+
+        if (!validation.success) {
+            return NextResponse.json(validation.error.errors, { status: 400 });
+        }
+
+        const newIssue = await prisma.issue.create({
+            data: { title: body.title, Description: body.description }, // Update property names here
+        });
+
+        return NextResponse.json(newIssue, { status: 201 });
+    } catch (error) {
+        console.error("Error in POST request:", error);
+        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    }
+}
