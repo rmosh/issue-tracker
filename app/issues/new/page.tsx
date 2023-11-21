a3d9cf445963f22d743b4b5332eb70634eeed7dc
Moshtaq <moshtaqramazan@gmail.com>
2023-11-21 17:52:57 +0100

    Handling submission form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,25 +1,60 @@
 'use client';
+// Import necessary modules and components
+import React, { useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
 import { Button, TextField } from '@radix-ui/themes';
 import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
+import axios, { AxiosError } from 'axios';
+import { useRouter } from 'next/navigation';
 
+// Define the component
 const NewIssuePage = () => {
+  interface IssueForm {
+    title: string;
+    description: string;
+  }
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const router = useRouter();
+
+  // Render the form with error message
   return (
-    <div className="max-w-xl  space-y-3">
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post('/api/issues', data);
+        router.push('/issues');
+      })}
+    >
       <TextField.Root>
         <TextField.Slot>
           <MagnifyingGlassIcon
-            height="16"
-            width="16"
+            aria-hidden="true"
+            focusable="false"
           />
         </TextField.Slot>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input
+          placeholder="Title"
+          {...register('title')}
+        />
       </TextField.Root>
-      <SimpleMDE placeholder="Description" />
+
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE
+            placeholder="Description"
+            {...field}
+          />
+        )}
+      />
+
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
+// Export the component
 export default NewIssuePage;
