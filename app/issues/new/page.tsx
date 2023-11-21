74cbf4754d3cd7b95e9135f6da7f020ca566075a
Moshtaq <moshtaqramazan@gmail.com>
2023-11-21 18:48:32 +0100

    Error handling of the submission form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,8 +2,8 @@
 // Import necessary modules and components
 import React, { useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
-import { Button, TextField } from '@radix-ui/themes';
-import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
+import { Button, Callout, TextField } from '@radix-ui/themes';
+import { InfoCircledIcon, MagnifyingGlassIcon } from '@radix-ui/react-icons';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import axios, { AxiosError } from 'axios';
@@ -17,42 +17,57 @@ const NewIssuePage = () => {
   }
   const { register, control, handleSubmit } = useForm<IssueForm>();
   const router = useRouter();
+  const [error, setError] = useState('');
 
   // Render the form with error message
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post('/api/issues', data);
-        router.push('/issues');
-      })}
-    >
-      <TextField.Root>
-        <TextField.Slot>
-          <MagnifyingGlassIcon
-            aria-hidden="true"
-            focusable="false"
+    <div className="max-w-xl space-y-3">
+      {error && (
+        <Callout.Root color="red">
+          <Callout.Icon>
+            <InfoCircledIcon />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className=""
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post('/api/issues', data);
+            router.push('/issues');
+          } catch (error) {
+            setError('Something went wrong. Please try again ');
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Slot>
+            <MagnifyingGlassIcon
+              aria-hidden="true"
+              focusable="false"
+            />
+          </TextField.Slot>
+          <TextField.Input
+            placeholder="Title"
+            {...register('title')}
           />
-        </TextField.Slot>
-        <TextField.Input
-          placeholder="Title"
-          {...register('title')}
+        </TextField.Root>
+
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE
+              placeholder="Description"
+              {...field}
+            />
+          )}
         />
-      </TextField.Root>
 
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE
-            placeholder="Description"
-            {...field}
-          />
-        )}
-      />
-
-      <Button>Submit New Issue</Button>
-    </form>
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
